833879b466df1e3d10a026caf18e45c688d41216
Karim El-Naggar <karimfarid.naggar@gmail.com>
2021-03-24 22:29:30 +0200

    Initialize current accounts service
    Motivation:
    
    In order to create a current accounts service, we need to create a new spring boot application.
    
    Modifications:
    
    - User spring initializr to bootstrap a new spring boot application
    - Copy the newly generated spring boot application inside the mini bank repository
    
    Result:
    
    We can start the development of current accounts service.

---
+++ currentaccounts/src/main/java/com/karimelnaggar/currentaccounts/CurrentaccountsApplication.java
@@ -0,0 +1,13 @@
+package com.karimelnaggar.currentaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentaccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentaccountsApplication.class, args);
+	}
+
+}
---
+++ currentaccounts/src/test/java/com/karimelnaggar/currentaccounts/CurrentaccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karimelnaggar.currentaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentaccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
