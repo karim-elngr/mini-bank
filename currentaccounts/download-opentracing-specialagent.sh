e282b87b705f4499df575d6cf5f2ecfd70948974
Karim El-Naggar <karimfarid.naggar@gmail.com>
2021-04-04 19:47:27 +0200

    feat: add script to download opentracing specialagent

---
+++ currentaccounts/download-opentracing-specialagent.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O opentracing-specialagent-1.7.4.jar "https://repo1.maven.org/maven2/io/opentracing/contrib/specialagent/opentracing-specialagent/1.7.4/opentracing-specialagent-1.7.4.jar"
